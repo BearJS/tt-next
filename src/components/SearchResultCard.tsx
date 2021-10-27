ea1e1f91fa4efdaac8b9ee1f77f6ec5783f14231
Paul Liu <oucl5976@gmail.com>
2021-10-27 19:04:06 +0100

    tag

--- src/components/SearchResultCard.tsx
+++ src/components/SearchResultCard.tsx
@@ -2,7 +2,27 @@ import * as React from 'react';
 import {FC, useState} from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  ${(props) => props.theme.border};
+  margin-bottom: 8px;
+  padding: 1em;
+`;
+
+const Tag = styled.span<{wrapperType: iTunesWrapperType}>`
+  ${(props) => props.theme.border};
+  ${(props) => props.theme.borderRadius};
+  padding: 2px;
+  margin-bottom: 3px;
+  ${(props) =>
+    props.wrapperType === 'track' &&
+    `background: ${props.theme.notificationStyles.success};`};
+  ${(props) =>
+    props.wrapperType === 'collection' &&
+    `background: ${props.theme.notificationStyles.warning};`};
+  ${(props) =>
+    props.wrapperType === 'artist' &&
+    `background: ${props.theme.notificationStyles.error};`};
+`;
 
 const SearchResultCard: FC<{data: iTunesSearchResult}> = ({data}) => {
   const [expanded, setExpanded] = useState(false);
@@ -11,8 +31,20 @@ const SearchResultCard: FC<{data: iTunesSearchResult}> = ({data}) => {
     setExpanded(!expanded);
   };
 
+  const {artistName, collectionName, trackName, wrapperType} = data;
   return (
     <Container>
+      <Tag wrapperType={wrapperType}>{wrapperType}</Tag>
+      <img src="" alt="" />
+      <p>
+        <strong>Artist:</strong> {artistName}
+      </p>
+      <p>
+        <strong>Collection:</strong> {collectionName}
+      </p>
+      <p>
+        <strong>Track:</strong> {trackName}
+      </p>
       <pre>{JSON.stringify(data, null, 4)}</pre>
     </Container>
   );
--- src/components/forms/Fieldset.tsx
+++ src/components/forms/Fieldset.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const FieldsetStyles = styled.fieldset<FieldsetStyleProps>`
   margin-bottom: 8px;
-  border: 1px solid #ddd;
+  ${(props) => props.theme.border};
   ${(props) => props.disabled && `opacity: 0.5`};
   ${(props) => props.inline && `display: flex;`};
 `;
